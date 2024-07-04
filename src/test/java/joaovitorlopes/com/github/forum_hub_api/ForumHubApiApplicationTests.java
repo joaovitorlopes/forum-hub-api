09d66b2bcfec63d8e2cef09b4634cbecdf0b40ef
joaovitorlopes <jvlaaa+github@gmail.com>
2024-07-04 16:09:17 -0400

    first commit

---
+++ src/test/java/joaovitorlopes/com/github/forum_hub_api/ForumHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package joaovitorlopes.com.github.forum_hub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
